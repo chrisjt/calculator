f8c7f4378ad9d73104930da83b265225138d2b6c
chrisjt <chrisjoshtan@gmail.com>
2020-03-22 14:51:56 +1100

    Handle clicking for all buttons.

--- public/js/script.js
+++ public/js/script.js
@@ -3,20 +3,92 @@ $( document ).ready(function() {
 });
 
 var calcOutPrev = 0;
-var calcOut = 1;
-var btnOutClicked = false;
+var calcOut = 0;
+var btnClearClicked = false;
 
 // Function to change output on click.
 function updateCalc() {
   console.log("updateCalc() called.")
   $("#out").text(calcOut);
 }
+// Click functions.
+$(document).ready(function(){
+  $("#btnClear").click(function() {
+    console.log("btnClear clicked.")
+    calcOut = 0;
+    if(btnClearClicked == false) {
 
-$("#btnClear").click(function() {
-  console.log("btnClear clicked.")
-  calcOut = 0;
-  if(btnOutClicked == false) {
-
-  }
-  updateCalc();
+    }
+    updateCalc();
+  });
+  $("#btnPolarity").click(function() {
+  });
+  $("#btnPercent").click(function() {
+  });
+  $("#btnDivide").click(function() {
+  });
+  $("#btnMulti").click(function() {
+  });
+  $("#btnMinus").click(function() {
+  });
+  $("#btnPlus").click(function() {
+  });
+  $("#btnEqualsr").click(function() {
+  });
+  $(".btn-calc").click(function() {
+    console.log(".btn-calc clicked.")
+    var id = this.id;
+    switch(id) {
+      case "btnNine":
+        console.log("Nine pressed.")
+        calcOut = 9;
+        updateCalc();
+        break;
+      case "btnEight":
+        console.log("Eight pressed.")
+        calcOut = 8;
+        updateCalc();
+        break;
+      case "btnSeven":
+        console.log("Seven pressed.")
+        calcOut = 7;
+        updateCalc();
+        break;
+      case "btnSix":
+        console.log("Six pressed.")
+        calcOut = 6;
+        updateCalc();
+        break;
+      case "btnFive":
+        console.log("Five pressed.")
+        calcOut = 5;
+        updateCalc();
+        break;
+      case "btnFour":
+        console.log("Four pressed.")
+        calcOut = 4;
+        updateCalc();
+        break;
+      case "btnThree":
+        console.log("Three pressed.")
+        calcOut = 3;
+        updateCalc();
+        break;
+      case "btnTwo":
+        console.log("Two pressed.")
+        calcOut = 2;
+        updateCalc();
+        break;
+      case "btnOne":
+        console.log("One pressed.")
+        calcOut = 1;
+        updateCalc();
+        break;
+      case "btnZero":
+        console.log("Zero pressed.")
+        calcOut = 0;
+        updateCalc();
+        break;
+    }
+  });
 });
